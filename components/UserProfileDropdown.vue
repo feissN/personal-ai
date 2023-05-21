24b2149a946011e65b33c1ae224a208b92ca19d8
Norman <>
2023-05-21 18:19:02 +0200

    removed settings page

--- components/UserProfileDropdown.vue
+++ components/UserProfileDropdown.vue
@@ -1,9 +1,6 @@
 <template>
     <div v-if="userState.user">
-        <div
-            class="anchor cursor-pointer relative"
-            @click="showDropdown = !showDropdown"
-        >
+        <div class="anchor cursor-pointer relative" @click="showDropdown = !showDropdown">
             <img
                 v-if="userState.user.photoURL"
                 :src="userState.user.photoURL"
@@ -14,32 +11,13 @@
                 {{ userState.user.displayName }}
             </div>
 
-            <div
-                v-if="showDropdown"
-                class="dropdown absolute right-0 top-10 z-50 bg-[#444] w-32"
-            >
+            <div v-if="showDropdown" class="dropdown absolute right-0 top-10 z-50 bg-[#444] w-32">
                 <ul class="flex flex-col">
-                    <li
-                        class="px-4 py-2 hover:bg-[#353535]"
-                        @click="router.push('/')"
-                    >
-                        Chatbot
-                    </li>
-                    <li
-                        class="px-4 py-2 hover:bg-[#353535]"
-                        @click="router.push('profile')"
-                    >
+                    <li class="px-4 py-2 hover:bg-[#353535]" @click="router.push('/')">Chatbot</li>
+                    <li class="px-4 py-2 hover:bg-[#353535]" @click="router.push('profile')">
                         Profile
                     </li>
-                    <li
-                        class="px-4 py-2 hover:bg-[#353535]"
-                        @click="router.push('settings')"
-                    >
-                        Settings
-                    </li>
-                    <li class="px-4 py-2 hover:bg-[#353535]" @click="logout">
-                        Logout
-                    </li>
+                    <li class="px-4 py-2 hover:bg-[#353535]" @click="logout">Logout</li>
                 </ul>
             </div>
         </div>
